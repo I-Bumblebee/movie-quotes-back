d599f3ad60a10eaa6553e97d53c1faa2b5271643
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-05-10 14:26:24 +0400

    feat: add custom guest middleware
    - returns json response instead of redirect response

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\OAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('guest')->group(function () {
+	Route::prefix('oauth')->group(function () {
+		Route::get('/google/redirect', [OAuthController::class, 'getGoogleRedirectUrl'])->name('oauth.google.redirectUrl');
+		Route::get('/google/callback', [OAuthController::class, 'getGoogleCallback'])->name('oauth.google.callback');
+	});
+});
